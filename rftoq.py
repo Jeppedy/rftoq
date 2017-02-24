4a1ce8a6a79151af0caab08b2db7e039d3614f11
Jeffrey Herr <jeff@theherrs.com>
2017-02-24 11:54:46 -0600

    Cleaned up and working

--- rftoq.py
+++ rftoq.py
@@ -8,13 +8,10 @@
 import os
 import sys
 import time
-from datetime import datetime
-import pytz
-import tzlocal
-import requests
-import json
+#from datetime import datetime
+#import pytz
+#import tzlocal
 import RPi.GPIO as GPIO
-import sqlite3
 import paho.mqtt.client as mqtt
 
 from nrf24 import NRF24
@@ -24,19 +21,16 @@ from nrf24 import NRF24
 ##PUSHMON_ID="WmpnHI"
 ##PUSHINGBOX_URL="http://api.pushingbox.com/pushingbox"
 ##PUSHINGBOX_ID="vF6098C58E4A4D96"
+#GROVESTREAMS_URL = "http://grovestreams.com/api/feed?asPut&api_key=521dfde4-e9e2-36b6-bf96-18242254873f"
 
-GROVESTREAMS_URL = "http://grovestreams.com/api/feed?asPut&api_key=521dfde4-e9e2-36b6-bf96-18242254873f"
-
-DBLOCATION="/media/nas-rpi/TempMonData/rfmonDB.db"
-
-DEFAULT_API_KEY = "IjPjyGRBNX4215uvu7sAB86NBjCtklQByFAIb1VoJT2TUeXF"
-DEFAULT_FEED_ID = "1785749146"
+#DEFAULT_API_KEY = "IjPjyGRBNX4215uvu7sAB86NBjCtklQByFAIb1VoJT2TUeXF"
+#DEFAULT_FEED_ID = "1785749146"
 
 Q_BROKER="m11.cloudmqtt.com"
 Q_PORT=19873
+Q_USER="prcegtgc"
+Q_PSWD="7frPa1U_VXqA"
 Q_TOPIC="hello"
-
-# REMEMBER: OneWire is ALWAYS on Pin #4
 
 ##  RF Read Code
 pipes = [[0xF0, 0xF0, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xD1]]
@@ -74,7 +68,12 @@ def initRadioReceive():
 
     radio.startListening()
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code [%d]" % rc)
 
+def on_publish(client,userdata,mid):
+    print("Data Published, Msg ID: [%d]" % mid)
+    pass
 
 # main program entry point - runs continuously updating our datastream with the
 def run():
@@ -82,49 +81,26 @@ def run():
   init_GPIO()
   initRadioReceive()
 
+  client = mqtt.Client("rftoq")
+  client.on_publish = on_publish
+  client.on_connect = on_connect
+  client.username_pw_set(Q_USER, Q_PSWD)
+
   while True:
     pipe = [1]
     while( radio.available(pipe, False) ):
         recv_buffer = []
-        myDateTime = datetime.utcnow().replace(tzinfo=pytz.utc);
+        recv_string = ""
+        #myDateTime = datetime.utcnow().replace(tzinfo=pytz.utc);
 
         radio.read(recv_buffer)
-	#print "[%s]" % recv_buffer
+        for x in recv_buffer:
+            recv_string += chr(x)
+	print "RF Msg received: [%s]" % recv_string
 
-        doPublish = False
-
-
-        doPublish = True
-
-        if( doPublish ):
-            print "- Updating"
-
-            def on_connect(client, userdata, flags, rc):
-                print("Connected with result code [%d]" % rc)
-
-            def on_publish(client,userdata,mid):
-                print("Data Published, Msg ID: [%d]" % mid)
-                pass
-
-            client = mqtt.Client("rftoq")
-            client.on_publish = on_publish
-            client.on_connect = on_connect
-
-            client.username_pw_set("prcegtgc", "7frPa1U_VXqA")
-            client.connect(Q_BROKER, Q_PORT,  keepalive=60)
-
-            recv_string = ""
-            for x in recv_buffer:
-                recv_string += chr(x)
-
-            client.publish(Q_TOPIC,recv_string)
-            client.disconnect()
-
-
-            doPublish = False
-        else:
-            print "- TOO SOON"
-
+        client.connect(Q_BROKER, Q_PORT, keepalive=60)
+        client.publish(Q_TOPIC,recv_string)
+        client.disconnect()
 
     radio.startListening()
 
