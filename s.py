99ae4aa7e6d8142591d6ea47e267a53888dd7edf
Jeffrey Herr <jeff@theherrs.com>
2017-02-25 08:15:59 -0600

    moved samples to different directory

--- s.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import paho.mqtt.client as mqtt
-
-mqttc = mqtt.Client("python_pub")
-mqttc.username_pw_set("prcegtgc", "7frPa1U_VXqA")
-mqttc.connect("m11.cloudmqtt.com", 19873)
-mqttc.publish("hello", "Hello, World!")
-mqttc.loop(2)
